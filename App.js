3efb1e489035315a412de2d7eb3adde84d2f36e1
Aryan Gupta <aryangupta007yo@gmail.com>
2021-03-07 16:49:49 +0530

    caching

--- App.js
+++ App.js
@@ -18,14 +18,16 @@ import ImageInput from "./app/components/ImageInput";
 import { NavigationContainer } from "@react-navigation/native";
 import AuthNavigator from "./app/navigation/AuthNavigator";
 import AppNavigator from "./app/navigation/AppNavigator";
-
+import OfflineNotice from "./app/components/OfflineNotice"
 
 export default function App() {
 
   return (
-   
+   <>
+   <OfflineNotice/>
   <NavigationContainer>
    <AppNavigator/>
   </NavigationContainer>
+  </>
   );
 }
